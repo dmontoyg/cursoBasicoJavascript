3200c55520e6a768dd786140903d5137ba25c762
Diego Montoya <dmontoyg@gmail.com>
2022-04-15 22:07:56 -0500

    Refuerzo de conocimieintos JS

---
+++ 1-valores.js
@@ -0,0 +1,47 @@
+// Tipos primitivos
+
+40   // Numeros 
+3.14
+
+"Diego Montoya"  // Strings (cadenas de texto), Esto es la forma en la que puedo crear algo que se pueda imprimir en la pantalla, o escribir información, que el usuario pueda entender y leer. utilizar palabras, o frases.
+
+'Diego Montoya'
+
+true   // Booleanos, estos fueron inventados por George Boole, un Matemático británico. y esto hace referencia a cosas que podamos validar.
+false
+
+null   // Valores vacios (Empty values), es como un placeholder (espacio reservado para algun valor) de algún valor faltante.
+
+undefined
+
+
+ /* ================================================================ */
+
+ // Tipos de objetos o no primitivos
+
+ [1, 2, 3, 4] // corchetes // arrays
+ { nombre: 'Diego' }  // llaves // objetos
+
+ /* ================================================================ */
+
+Tipos:
+
+Number
+Boolean
+String
+undefined
+null
+Object
+Symbol
+
+// typeof es un tipo de operador unitario "Unary operator" que trabaja con 1 operando (variable)
+
+var nombre; 
+
+typeof 30 
+typeof true
+typeof "Diego" 
+typeof nombre 
+typeof null
+typeof {}
+typeof []
---
+++ 2-variables.js
@@ -0,0 +1,18 @@
+var nombre = "Diego";
+
+// declarar
+var edad;
+
+//inicializar
+edad = 25;
+
+edad
+
+var elementos = ["computador", "celular"];
+
+var persona = {
+    nombre: "Diego",
+    edad: 30
+}
+
+persona;
